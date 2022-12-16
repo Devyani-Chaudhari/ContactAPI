572cf27c4f7a6a7dfeba78059b052efb683605b3
Devyani Chaudhari <devyani.chaudhari@emtecinc.com>
2022-12-16 10:44:37 +0530

    Add project files.

---
+++ Demo_Project/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Demo_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo_Project.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
